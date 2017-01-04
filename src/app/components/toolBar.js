86aa663f299ac7e6072525c71593a113ff20c152
eXt-Ra <vanardois.romain@gmail.com>
2017-01-04 23:44:54 +0100

    mise en place de la toolbar multiple

--- src/app/components/toolBar.js
+++ src/app/components/toolBar.js
@@ -1,5 +1,13 @@
 class ToolBarController {
-  constructor($scope, $log, api) {
+  constructor($scope, $log, api, $element) {
+    $scope.clearSearchTerm = () => {
+      $scope.searchTerm = '';
+    };
+    // The md-select directive eats keydown events for some quick select
+    // logic. Since we have a search input here, we don't need that logic.
+    $element.find('input').on('keydown', ev => {
+      ev.stopPropagation();
+    });
     // Get Chauffeurs de la societe
     api.loadChauffeurs('73').then(chauffeurs => {
       $scope.chauffeurs = angular.fromJson(chauffeurs);

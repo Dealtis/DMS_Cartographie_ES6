0dc6485a11771eda40f4d285da95e98fb39dfc7b
eXt-Ra <vanardois.romain@gmail.com>
2017-01-05 14:52:17 +0100

    finish toolbar

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {getPositionsFun} from './app/functions/getPositionsFun';
 import 'angular-ui-router';
 import 'angular-material';
 import 'lodash';
+import moment from 'moment';
 // Dependencies google maps
 import 'angular-google-maps';
 import 'angular-simple-logger';
@@ -24,6 +25,22 @@ export const app = 'app';
 angular
   .module(app, ['ui.router', 'ngMaterial', 'uiGmapgoogle-maps'])
   .config(routesConfig)
+  .config($mdDateLocaleProvider => {
+    $mdDateLocaleProvider.months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+    $mdDateLocaleProvider.shortMonths = ['janv', 'févr', 'mars', 'avri', 'mai', 'juin', 'juill', 'août', 'sept', 'octo', 'nove', 'déce'];
+    $mdDateLocaleProvider.days = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+    $mdDateLocaleProvider.shortDays = ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'];
+    $mdDateLocaleProvider.firstDayOfWeek = 1;
+    $mdDateLocaleProvider.formatDate = date => {
+      return moment(date).format('DD/MM/YYYY');
+    };
+    $mdDateLocaleProvider.weekNumberFormatter = weekNumber => {
+      return `Semaine ${weekNumber}`;
+    };
+
+    $mdDateLocaleProvider.msgCalendar = 'Calendrier';
+    $mdDateLocaleProvider.msgOpenCalendar = 'Ouvrir le calendrier';
+  })
   .config(uiGmapGoogleMapApiProvider => uiGmapGoogleMapApiProvider.configure({
     key: 'AIzaSyBraT3buBPTdDmqZ-Urn81aI8zWTfttA2Y',
     libraries: 'weather,geometry,visualization'
